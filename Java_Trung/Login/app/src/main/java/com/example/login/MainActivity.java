6343edca5b4ce05a028effebf8b9c8e3d7017f00
KhacTrung <trungnk153@gmail.com>
2024-05-17 15:11:11 +0700

    Login, login fb & gg, logout, start activity

---
+++ Java_Trung/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView fbBtn;
+    CallbackManager callbackManager;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView googleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button loginButton = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.editTextText3);
+
+        loginButton.setOnClickListener(v -> {
+            String username = editText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("USERNAME", username);
+            startActivity(intent);
+            finish();
+        });
+
+        TextView text = findViewById(R.id.textView4);
+        text.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, SignUpActivity.class)));
+
+        //Facebook
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Toast.makeText(MainActivity.this, "Facebook login canceled", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull FacebookException error) {
+                        // App code
+                        Toast.makeText(MainActivity.this, "Facebook login error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        fbBtn = findViewById(R.id.imageView20);
+        fbBtn.setOnClickListener(v -> {
+            //login by facebook
+            signInWithFacebook();
+        });
+
+        //Google
+        googleBtn = findViewById(R.id.imageView22);
+        googleBtn.setOnClickListener(v -> {
+            //login by google
+            signInWithGoogle();
+        });
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+    }
+    // Login facebook
+    void signInWithFacebook() {
+        LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Collections.singletonList("public_profile"));
+    }
+
+    // Login google
+    void signInWithGoogle() {
+        gsc.signOut().addOnCompleteListener(this, task -> {
+            Intent signInIntent = gsc.getSignInIntent();
+            startActivityForResult(signInIntent, 1000);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        //Login facebook
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Login google
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("USERNAME", account.getDisplayName());
+                    intent.putExtra("EMAIL", account.getEmail());
+                    startActivity(intent);
+                }
+            } catch (ApiException e) {
+                Toast.makeText(getApplication(),  "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ Java_Trung/Login/app/src/main/java/com/example/login/SecondActivity.java
@@ -0,0 +1,91 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import org.json.JSONException;
+
+public class SecondActivity extends AppCompatActivity {
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // Get username and email from Intent
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("USERNAME");
+        String email = intent.getStringExtra("EMAIL");
+
+        // Display username and email on TextViews
+        TextView usernameTextView = findViewById(R.id.textView5);
+        TextView emailTextView = findViewById(R.id.textView6);
+        usernameTextView.setText(username);
+        emailTextView.setText(email);
+
+        //Graph API
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken!=null && !accessToken.isExpired()) {
+            GraphRequest request = GraphRequest.newMeRequest(
+                    accessToken,
+                    (object, response) -> {
+                        // Application code
+                        try {
+                            assert object != null;
+                            String name = object.getString("name");
+                            usernameTextView.setText(name);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,link");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
+        // Set up sign out button
+        Button logoutButton = findViewById(R.id.button2);
+        logoutButton.setOnClickListener(v -> signOut());
+        logoutButton.setOnClickListener(v ->{
+            LoginManager.getInstance().logOut();
+            startActivity(new Intent(SecondActivity.this,MainActivity.class));
+            finish();
+        });
+
+        // Set up Google sign-in client
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this, gso);
+    }
+
+    // Method to sign out from Google account
+    private void signOut() {
+        gsc.signOut()
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign out successful, navigate back to MainActivity
+                        Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Sign out failed
+                        Toast.makeText(SecondActivity.this, "Sign out failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
