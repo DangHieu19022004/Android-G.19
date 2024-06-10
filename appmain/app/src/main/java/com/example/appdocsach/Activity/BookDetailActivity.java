39caa74fd91dd5599237c3d04eb4b154450e019f
KhacTrung <trungnk153@gmail.com>
2024-06-10 18:26:53 +0700

    Demo interactive like and dislike button

--- appmain/app/src/main/java/com/example/appdocsach/Activity/BookDetailActivity.java
+++ appmain/app/src/main/java/com/example/appdocsach/Activity/BookDetailActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.appdocsach.R;
 
@@ -19,16 +19,12 @@ public class BookDetailActivity extends AppCompatActivity {
 
     ImageView threeDotsButton;
     Button btnstartreadDetail;
+    LinearLayout likeDetail, dislikeDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_book_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         mapping();
 
@@ -50,6 +46,32 @@ public class BookDetailActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        // Xử lý sự kiện khi nhấn nút like và dislike
+        likeDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BookDetailActivity.this, "Liked!", Toast.LENGTH_SHORT).show();
+                changeButtonColor(likeDetail);
+            }
+        });
+
+        dislikeDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BookDetailActivity.this, "Disliked!", Toast.LENGTH_SHORT).show();
+                changeButtonColor(dislikeDetail);
+            }
+        });
+    }
+
+    //Đổi màu khi nhấn nút
+    private void changeButtonColor(LinearLayout buttonLayout) {
+        // Khai báo phần image
+        ImageView imageView = (ImageView) buttonLayout.getChildAt(0);
+
+        // Đổi màu
+        imageView.setColorFilter(getResources().getColor(R.color.colorGreen));
     }
 
     private void showPopupmenu() {
@@ -60,5 +82,7 @@ public class BookDetailActivity extends AppCompatActivity {
     private void mapping(){
         threeDotsButton = findViewById(R.id.threeDotsButton);
         btnstartreadDetail = findViewById(R.id.btnstartreadDetail);
+        likeDetail = findViewById(R.id.likeDetail);
+        dislikeDetail = findViewById(R.id.dislikeDetail);
     }
 }
